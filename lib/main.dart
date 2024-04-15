247b45233eecdbb85794f1e50fb7700b7e5aef85
Eric D. Frazer <EricFrazer02@gmail.com>
2024-04-15 13:01:42 -0600

    Initial Commit- Added Calendar Feature

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Calendar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TaskCalendar(),
+    );
+  }
+}
+
+class TaskCalendar extends StatefulWidget {
+  @override
+  _TaskCalendarState createState() => _TaskCalendarState();
+}
+
+class _TaskCalendarState extends State<TaskCalendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task Calendar'),
+      ),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2010, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay; // update `_focusedDay` here as well
+          });
+        },
+      ),
+    );
+  }
+}

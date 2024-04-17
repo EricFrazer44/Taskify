6dc51e0206a699d41145d164a2ad21e1843a11c8
Eric D. Frazer <EricFrazer02@gmail.com>
2024-04-17 00:35:24 -0600

    Added Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:taskify/authentication.dart';
 import 'firebase_options.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,6 +37,7 @@ class TaskCalendar extends StatefulWidget {
 
 class _TaskCalendarState extends State<TaskCalendar> {
   final CalendarFormat _calendarFormat = CalendarFormat.month;
+  final _formKey = GlobalKey<FormBuilderState>();
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
@@ -40,7 +45,7 @@ class _TaskCalendarState extends State<TaskCalendar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task Calendar'),
+        title: const Text('Taskify'),
       ),
       body: TableCalendar(
         firstDay: DateTime.utc(2010, 10, 16),
@@ -53,10 +58,113 @@ class _TaskCalendarState extends State<TaskCalendar> {
         onDaySelected: (selectedDay, focusedDay) {
           setState(() {
             _selectedDay = selectedDay;
-            _focusedDay = focusedDay; // update `_focusedDay` here as well
+            _focusedDay = focusedDay;
           });
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final _formKey = GlobalKey<FormState>();
+          String? description;
+          DateTime? date;
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Add Task'),
+                content: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Description'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a description';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            description = value;
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          child: const Text('Select Date/Time'),
+                          onPressed: () async {
+                            final selectedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2100),
+                            );
+                            if (selectedDate != null) {
+                              final selectedTime = await showTimePicker(
+                                context: context,
+                                initialTime: TimeOfDay.now(),
+                              );
+                              if (selectedTime != null) {
+                                date = DateTime(
+                                  selectedDate.year,
+                                  selectedDate.month,
+                                  selectedDate.day,
+                                  selectedTime.hour,
+                                  selectedTime.minute,
+                                );
+                              }
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Add'),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        addTask(description!, date!);
+                        Navigator.of(context).pop();
+                      }
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
     );
+  }
+
+  Future<void> addTask(String description, DateTime date) async {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+    String dateStr = DateFormat('yyyy-MM-dd').format(date);
+    String timeStr = DateFormat('HH:mm:ss').format(date);
+
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    return users
+        .doc(userId)
+        .collection('tasks')
+        .doc(dateStr)
+        .set({}, SetOptions(merge: true))
+        .then((_) => users.doc(userId).collection('tasks').doc(dateStr).update({
+              timeStr: {'description': description, 'time': timeStr},
+            }))
+        .then((value) => print('Task Added'))
+        .catchError((error) => print('Failed to add task: $error'));
   }
 }
